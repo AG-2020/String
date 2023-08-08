792f6da24b0a261fa40cc1eb25f53405879dc2ed
Aidan <55570210+AXL-2020@users.noreply.github.com>
2023-08-08 11:47:15 -0400

    Update string.hpp

--- string.hpp
+++ string.hpp
@@ -7,13 +7,7 @@
 //Description: Interface definition of String class for use with test oracle.
 //             For use with Project 2, milestone 2, 3, 4.
 //
-// To use the supplied test oracles you will need to use this interface and namings.
-// You must also have all of these methods and functions defined for your string class.
-//
-// You can NOT add any attributes/members.  If you do the test oracles will not work.
-//
-// You need to implement all of the methods and funcitons declared here.
-//
+// HEADER FILE THAT PROFESSOR PROVIDED AS TEMPLATE FOR CODE
 
 #ifndef CS23001_STRING_INTERFACE_HPP
 #define CS23001_STRING_INTERFACE_HPP
